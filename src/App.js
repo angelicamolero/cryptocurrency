90b82554386f07d6e200109e1b0f173b9bbae091
angelicamolero <angelicamoleror8@gmail.com>
2021-01-06 12:26:59 -0300

    created form file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import image from './cryptocurrency.png';
+import Form from './components/Form';
 
 const Container = styled.div`
   max-width: 900px;
   margin: 0 auto;
+
   @media (min-width: 992px){
     display: grid;
-    grid-template-columns: repeat(2, 1)fr;
+    grid-template-columns: repeat(2, 1fr);
     column-gap: 2rem;
   }
 `;
@@ -46,6 +48,7 @@ function App() {
           <Heading>
             Check Cryptocurrency
           </Heading>
+          <Form/>
         </div>
       </Container>
   );
