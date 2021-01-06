b45f13cc5f2058b6bbd0c79e0ddb72b389bb042a
angelicamolero <angelicamoleror8@gmail.com>
2021-01-06 11:58:06 -0300

    adding two columns

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import image from './cryptocurrency.png';
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1)fr;
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    background-color: #66A2FE;
+    display: block;
+  }
+`;
 
 function App() {
   return (
-      <h1>cryptocurrency</h1>
+      <Container>
+        <div>
+          <Image
+          src={image}
+          />
+        </div>
+        <div>
+          <Heading>
+            Check Cryptocurrency
+          </Heading>
+        </div>
+      </Container>
   );
 }
 
