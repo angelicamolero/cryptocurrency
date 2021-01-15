5dbefaf31eae4bbe85e75c9841ae8e5ffd4ba41b
angelicamolero <angelicamoleror8@gmail.com>
2021-01-14 22:21:31 -0300

    adding result container

--- src/components/Stimator.js
+++ src/components/Stimator.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import styled from '@emotion/styled'
+
+const ResultDiv = styled.div `
+    color: #08234d;
+    background-color:#fff;
+    text-align: center;
+    padding: 2rem;
+    margin: 2rem 0;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Paragraph = styled.p `
+    font-size: 18px;
+
+    span {
+        font-weight: bold;
+    }
+`;
+
+const Precio = styled.p `
+    font-size: 30px;
+    span {
+        font-weight: bold;
+    }
+`;
 
 const Stimator = ({result}) => {
     
     if(Object.keys(result).length === 0) return null;
     return(
-    <div>
-        <p>The price is: <span>{result.PRICE}</span></p>
-        <p>Highest price today: <span>{result.HIGHDAY}</span></p>
-        <p>Lowest price today: <span>{result.LOWDAY}</span></p>
-        <p>Last 24 hours variation: <span>{result.CHANGEPCT24HOUR}</span></p>
-        <p>Last Update: <span>{result.LASTUPDATE}</span></p>
-    </div>
+    <ResultDiv>
+        <Precio>The price is: <span>{result.PRICE}</span></Precio>
+        <Paragraph>Highest price today: <span>{result.HIGHDAY}</span></Paragraph>
+        <Paragraph>Lowest price today: <span>{result.LOWDAY}</span></Paragraph>
+        <Paragraph>Last 24 hours variation: <span>{result.CHANGEPCT24HOUR}</span></Paragraph>
+        <Paragraph>Last Update: <span>{result.LASTUPDATE}</span></Paragraph>
+    </ResultDiv>
     );
 }
 
